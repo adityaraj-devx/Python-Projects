7ee350222b1005c9fb9d1362c62a9f74614a419a
adityaraj-devx <aadiiraj7780@gmail.com>
2026-02-08 17:08:35 +0530

    add setting fucntion added

---
+++ User Configuration Manager/main.py
@@ -0,0 +1,13 @@
+#settings
+test_settings = {
+    'theme': 'light',
+    'language': 'English',
+    'notifications': 'allow',
+}
+def add_setting(settings, new_settings):
+    key = new_settings(0).lower
+    value = new_settings(1).lower
+    if key in settings:
+        return f"Setting {key} already exists! Cannot add a new setting with this name."
+    else:
+        return f"Settings {key} added with value {value} successfully!"
