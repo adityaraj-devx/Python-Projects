2b99271902319fba074fe333f6021d65aa423e53
adityaraj-devx <aadiiraj7780@gmail.com>
2026-02-18 11:23:42 +0530

    added new methods

--- Email Simulator/main.py
+++ Email Simulator/main.py
@@ -9,6 +9,20 @@ class Email:
     def mark_as_read(self):
         self.read = True
 
+    def display_full_email(self):
+        self.mark_as_read()
+        print('\n--- Email ---')
+        print(f'From: {self.sender.name}')
+        print(f'To: {self.receiver.name}')
+        print(f'Subject: {self.subject}')
+        print(f'Body: {self.body}')
+        print('------------\n')
+
+    def __str__(self):
+        status = "Read" if self.read  else "Unread"
+        return f"[{status}] From: {self.sender.name} | Subject: {self.subject}"
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -24,3 +38,8 @@ class Inbox:
 
     def receive_email(self, email):
         self.emails.append(email)
+
+    def list_emails(self):
+        if not self.emails:
+            print('Your inbox is empty.\n')
+            return
