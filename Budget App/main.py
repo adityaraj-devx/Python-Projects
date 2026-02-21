819bc7e1032dcc91bff6ba93a4cf4341f1289ced
adityaraj-devx <aadiiraj7780@gmail.com>
2026-02-21 12:50:32 +0530

    started

---
+++ Budget App/main.py
@@ -0,0 +1,22 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({
+            "amount": amount,
+            "description": description
+        })
+    
+    def withdraw(self, amount, description=""):
+        if self.get_balance() >= amount:
+            self.ledger.append({
+                "amount": -amount,
+                "description": description
+            })
+            return True
+        return False
+
+    def get_balance(self):
+        return sum(item["amount"] for item in self.ledger)
